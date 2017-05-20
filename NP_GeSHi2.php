9ec85a455b2ba8f465219122faa8b5cd083980bc
yamamoto <yamamoto@kyms.jp>
2017-05-20 17:10:23 +0900

    Update NP_GeSHi2.php

--- NP_GeSHi2.php
+++ NP_GeSHi2.php
@@ -8,7 +8,7 @@
  *	It can hihglight code blocks with
  *	<pre class="languageName">...</pre>
  *	and inline code with
- *	<code class="languageName">...&lt;/code>
+ *	<code class="languageName">...</code>
  *	The pre and code tags will replaced with div and span tags and the enclosed
  *	code will be highlighted with the GeSHi library
  *	
@@ -27,11 +27,11 @@
  *			- built with GeSHi version 1.0.7.20
  *
  */
- 
+
 include_once('geshi/geshi.php');
  
 class NP_GeSHi2 extends NucleusPlugin {
- 
+
 	function getName() { return 'NP_GeSHi2'; }
 	function getAuthor()  { return 'Kai Greve'; }
 	function getURL()  { return 'http://kgblog.de/'; }
@@ -39,37 +39,37 @@ class NP_GeSHi2 extends NucleusPlugin {
 	function getDescription() {
 		return 'Integrates GeSHi - the Generic Syntax Highlighter - into Nucleus CMS. GeSHi can highlight code from several programming languages.';
 	}
- 
+
 	function getEventList() {
 		return array('PreItem', 'PreComment');
 	}
- 
+
 	function install() {
 		$this->createOption('pre_header','Header for pre','text','<div class="###language###">');
 		$this->createOption('pre_footer','Footer for pre','text','</div>');
- 
+		
 		$this->createOption('code_header','Header for code','text','<span class="###language###">');
 		$this->createOption('code_footer','Footer for code','text','</span>');
 	}
  
 	function geshi ($code) {
 		global $blog;
- 
+
 		// remove line breaks if the blog add them
 		if ($blog->settings['bconvertbreaks']==1) {
 			$code[4]=removeBreaks($code[4]);
 		}
- 
+    
 		$output='';
- 
+
 		$output=$code[4];
- 
+
 		// highlight the code with GeSHi		
 		$geshi =& new GeSHi($output, $code[2]);
- 
+		
 		$geshi->set_header_type(GESHI_HEADER_NONE);
 		$output=$geshi->parse_code();
- 
+  
 		if (stristr ($code[1], 'pre')) {
 			$my_header=$this->getOption('pre_header');
 			$my_footer=$this->getOption('pre_footer');
@@ -78,21 +78,21 @@ class NP_GeSHi2 extends NucleusPlugin {
 			$my_header=$this->getOption('code_header');
 			$my_footer=$this->getOption('code_footer');
 		}
- 
+  
 		// replace ###language### with the actual language
 		$my_header=str_replace('###language###', $code[2], $my_header);
- 
+  
   		// add header and footer to the code
 		$output=$my_header.$output.$my_footer;
- 
+  
 		return $output;  
 	}
  
 	function find_code($text) {
 		global $CONF, $blog;
- 
+
 		$text = preg_replace_callback('/(\<pre class=\")(.*)(\"\>)(.*)(\<\/pre\>)/Usi',  array(&$this, 'geshi'), $text);
- 
+     
 		$text = preg_replace_callback('/(\<code class=\")(.*)(\"\>)(.*)(\<\/code\>)/Usi',  array(&$this, 'geshi'), $text);
  
 		return $text;
@@ -106,7 +106,7 @@ class NP_GeSHi2 extends NucleusPlugin {
 	function event_PreComment($_data) {
 		$_data['comment']['body'] = $this->find_code($_data['comment']['body']);
 	}
- 
+
 	function supportsFeature ($what) {
 		switch ($what)
 		{
